8186213e21b08ca7fa60b5c31ab15b30d66b4ca3
Xudoynazar1112 <141805748+Xudoynazar1112@users.noreply.github.com>
2024-11-02 14:16:42 +0500

    updated main photo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import logo from "/images/logo.png";
 
 const Navbar = () => {
   return (
-    <div className="py-10 md:px-48 px-5 flex justify-between items-center w-full">
+    <div className="py-5 md:px-40 px-5 flex justify-between items-center w-full">
       <div className="flex items-center">
         <img src={logo} alt="logo" className="w-16" />
         <div className="md:flex md:flex-col text-black text-xl hidden">
@@ -13,7 +13,7 @@ const Navbar = () => {
         </div>
       </div>
       <div>
-        <a href="#contact" className="bg-[#FEC901] font-bold md:text-xl py-2 px-4 rounded-xl">↪ Ariza yuborish</a>
+        <a href="#contact" className="bg-[#FEC901] font-bold md:text-xl py-4 px-6 rounded-xl">↪ Ariza yuborish</a>
       </div>
     </div>
   );
--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -1,35 +1,52 @@
 import React from "react";
 import { RiCashFill } from "react-icons/ri";
 import hero from "/images/main-hero.png";
+import { SiTicktick } from "react-icons/si";
 
 const Section1 = () => {
   return (
     <div className="px-4">
-      <di data-aos="fade-up" className="flex justify-center items-center md:text-[5rem] text-4xl">
+      <di
+        data-aos="fade-up"
+        className="flex justify-center items-center md:text-[5rem] text-4xl"
+      >
         <p className="md:w-3/4 text-center md:leading-[5rem] leading-relaxed font-bold">
           <span className="sariq">34 Kun </span>ichida Savdolaringizni{" "}
           <span className="sariq">70% gacha</span> oshiring!
         </p>
       </di>
-      <div className="flex md:justify-between flex-col md:flex-row md:items-end items-center md:mt-0 mt-12">
+      <div className="flex md:justify-between md:gap-5 md:text-start text-center flex-col md:flex-row md:items-end items-center">
         {/* Left box */}
-        <div data-aos="fade-up" className="bg-slate-100 w-[20rem] h-[20rem] rounded-xl p-10 text-4xl">
+        <div
+          data-aos="fade-up"
+          className="bg-slate-100 md:w-[20rem] w-full h-[22rem] flex md:flex-row md:items-start flex-col items-center rounded-xl p-10 text-4xl"
+        >
           <RiCashFill className="text-[#FEC901] mb-10" />
           <p>Kechagidan ko'proq soting</p>
         </div>
         {/* Image box */}
-        <div data-aos="fade-up" className="relative md:mb-0 mb-5">
-          <img src={hero} alt="photo" className="md:w-[31rem] w-full" />
-          <p className="bg-white/75 md:w-72 w-40 md:h-16 h-10 absolute md:bottom-3 bottom-2 md:left-28 left-20 rounded-2xl text-center md:skew-x-[35deg] skew-x-[20deg]">
-            <h2 className="md:text-xl text-md">Latifov Izzatbek</h2>
-            <p className="md:text-base text-[10px]">Targetolog</p>
-          </p>
+        <div data-aos="fade-up" className="hero">
+          <img src={hero} alt="photo" />
+          <div className="bordered">
+            <span>izzat.digital <SiTicktick /></span>
+            <p className="hero-text">
+              <h2>Latifov Izzatbek</h2>
+              <p>Targetolog</p>
+            </p>
+          </div>
         </div>
         {/* Right box */}
-        <div data-aos="fade-up" className="bg-slate-100 w-[20rem] h-[20rem] rounded-xl p-10 text-xl flex flex-col gap-10 pr-14 font-semibold">
-          <p>O'ylab o'tirishga xojat yo'q deb nomlangan <span className="text-[#FEC901]">garantiya</span></p>
+        <div
+          data-aos="fade-up"
+          className="bg-slate-100 md:w-[20rem] w-full md:text-start text-center h-[22rem] rounded-xl p-10 text-xl flex flex-col gap-10 pr-14 font-semibold"
+        >
           <p>
-            Savdo hajmi yaxshi tarafga o'zgarmasa xizmat pulini <span className="text-[#FEC901]">100%</span> qaytaramiz!
+            O'ylab o'tirishga xojat yo'q deb nomlangan{" "}
+            <span className="text-[#FEC901]">garantiya</span>
+          </p>
+          <p>
+            Savdo hajmi yaxshi tarafga o'zgarmasa xizmat pulini{" "}
+            <span className="text-[#FEC901]">100%</span> qaytaramiz!
           </p>
         </div>
       </div>
